8a81ea10db7b5750538baeb9e9fb286ff48de1c9
hirudan <republiqueajamais@hotmail.com>
2020-05-30 01:01:23 -0700

    Baseline

---
+++ src/components/widget.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from 'react';
+
+export interface WidgetProps { name: string }
+
+export class Widget extends React.Component<WidgetProps, any>{
+    render(){
+        return(<h1>Hello, {this.props.name}!</h1>);
+    }
+}
---
+++ src/react.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Widget} from './components/widget'
+
+ReactDOM.render(<Widget name={"User"} />, document.getElementById('app'));
